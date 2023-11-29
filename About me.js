56efe8a77cdddafefd4ee31339e5f539a1dfd857
Oliver <owgibbs2009@gmail.com>
2023-11-29 15:17:15 -0700

    intial html and a function that shows and hides images

---
+++ About me.js
@@ -0,0 +1,11 @@
+var x = 20;
+var speed = 3;
+var movement;
+const animationEL = document.getElementById('Animation');
+animationEL.onclick(e => {
+    console.log({ target: e.target })
+
+
+})
+
+
---
+++ Index.js
@@ -0,0 +1,9 @@
+var x = 20;
+var speed = 3;
+
+draw = function () {
+    background(202, 202, 202)
+
+
+
+}
